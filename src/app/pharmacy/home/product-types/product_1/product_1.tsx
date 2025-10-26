af27f8a68fde69eb676c2a292e3e68f457639ece
VuHa05-Rek <nguyenvuhaaa123@gmail.com>
2025-10-27 00:53:03 +0700

    figbug

--- src/app/pharmacy/home/product-types/product_1/product_1.tsx
+++ src/app/pharmacy/home/product-types/product_1/product_1.tsx
@@ -4,7 +4,7 @@ import Image, { StaticImageData } from 'next/image';
 import { ProductContent } from '../../product-content/product-content';
 import Medicine from '../../../assets/medicine.png';
 import Next from '../../../assets/next.png';
-import './product_1.css';
+import styles from './product_1.module.css';
 import { SeeAllButton } from '../../../common/all-product-button/AllProductButton';
 interface Product {
   id: number;
@@ -27,21 +27,27 @@ const products: Product[] = [
 
 const Product_1: React.FC<ProductProps> = React.memo(({ prefix }) => {
   return (
-    <section className="container-product">
-      <div className="head-product">
+    <section className={styles['container-product']}>
+      <div className={styles['head-product']}>
         {['Pain Reliever, Fever Reducer', 'Cardiovascular', 'Nerve', 'Digestion, Liver', 'Eyes, Ears, Nose, Throat'].map((label, i) => (
           <button key={`${prefix}-label-${i}`}>{label}</button> 
         ))}
       </div>
 
-      <div className="product-list-wrapper">
-        <div className="product-list">
-          <div className="next-product1">
-            <Image src={Next} alt="next" width={40} height={40} />
+      <div className={styles['product-list-wrapper']}>
+        <div className={styles['product-list']}>
+          <div className={styles['next-product1']}>
+            <Image 
+              src={Next} 
+              alt="next" 
+              width={40} 
+              height={40}
+              priority
+            />
           </div>
 
           {products.map((product) => (
-            <div key={`${prefix}-product-${product.id}`} className="product-card"> {/* key duy nhất */}
+            <div key={`${prefix}-product-${product.id}`} className={styles['product-card']}>
               <ProductContent
                 image={product.image}
                 title={product.title}
@@ -50,13 +56,19 @@ const Product_1: React.FC<ProductProps> = React.memo(({ prefix }) => {
             </div>
           ))}
 
-          <div className="next-product2">
-            <Image src={Next} alt="next" width={40} height={40} />
+          <div className={styles['next-product2']}>
+            <Image 
+              src={Next} 
+              alt="next" 
+              width={40} 
+              height={40}
+              priority
+            />
           </div>
         </div>
       </div>
 
-      <div className="all-product">
+      <div className={styles['all-product']}>
         <SeeAllButton />
       </div>
     </section>
